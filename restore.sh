9e7bd478c0a5f65bd01fd1e2df516e889f935a52
Sonic <lucas@djls.io>
2025-09-08 21:49:41 -0400

    Replace B2 backup with S3 backup functionality

--- restore.sh
+++ restore.sh
@@ -7,15 +7,11 @@ set -e
 # - ETCD_DIR: the directory where the etcd data will be stored. Default: /etcd
 # - ETCD_BACKUP_DIR: the directory where the backup file will be stored. Default: /backups
 
-# Optional parameters for B2 push - if any of these are not set, the script will not get the backup from B2:
-# - B2_BUCKET_NAME: the name of the B2 bucket where the backup will be stored
-# - B2_APPLICATION_KEY_ID: the application key ID that will be used to authenticate with B2
-# - B2_APPLICATION_KEY: the application key that will be used to authenticate with B2
-
-# Future improvement:
-# - Check if the file exist before downloading it using the sha1 hash
-
-B2_ENABLED=1
+# Optionnal parameters for S3:
+# - S3_ENDPOINT: the S3 endpoint that will be used to push the backup
+# - S3_BUCKET_NAME: the name of the B2 bucket where the backup will be stored
+# - S3_ACCESS_KEY: the application key ID that will be used to authenticate with B2
+# - S3_SECRET_KEY: the application key that will be used to authenticate with B2
 
 # ------------------------------------------- #
 # Ensure that the required parameters are set #
@@ -51,28 +47,33 @@ fi
 # Ensure the backup directory exists or create it
 mkdir -p $ETCD_BACKUP_DIR
 
+
 # We check if any B2 parameters is not set
-if [ -z "$B2_BUCKET_NAME" ] || [ -z "$B2_APPLICATION_KEY_ID" ] || [ -z "$B2_APPLICATION_KEY" ]; then
-  echo "One or more of the B2_BUCKET_NAME, B2_APPLICATION_KEY_ID, B2_APPLICATION_KEY environment variables are not set."
+if [ -z "$S3_ENDPOINT" ] || [ -z "$S3_BUCKET_NAME" ] || [ -z "$S3_ACCESS_KEY" ] || [ -z "$S3_SECRET_KEY" ]; then
+  echo "One or more of the S3_ENDPOINT, S3_BUCKET_NAME, S3_ACCESS_KEY, S3_SECRET_KEY environment variables are not set."
   echo "Disabling B2 backup."
-  B2_ENABLED=0
+  S3_ENABLED=0
 fi
 
 BACKUP_FILE_TO_RESTORE=""
 
-if [ $B2_ENABLED -eq 1 ]; then
-  BACKUP_FILE_TO_RESTORE=$(b2 ls b2://$B2_BUCKET_NAME | tail -n 1)
+if [ $S3_ENABLED -eq 1 ]; then
+  # Set the S3 mc backend
+  export MC_HOST_S3=https://$S3_ACCESS_KEY:$S3_SECRET_KEY@$S3_ENDPOINT
+
+  # Read the latest backup
+  BACKUP_FILE_TO_RESTORE=$(mc ls S3/$S3_BUCKET_NAME | sort -k1,2 | tail -n 1 | awk '{print $NF}')
 
   # Get the file if it exists
   if [ -n "$BACKUP_FILE_TO_RESTORE" ]; then
     echo "Downloading the latest backup from B2:"
-    b2 file download b2://$B2_BUCKET_NAME/$BACKUP_FILE_TO_RESTORE $ETCD_BACKUP_DIR/$BACKUP_FILE_TO_RESTORE
+    mc cp S3/$S3_BUCKET_NAME/$BACKUP_FILE_TO_RESTORE $ETCD_BACKUP_DIR/$BACKUP_FILE_TO_RESTORE    
   else
     echo "No backup found in B2. Exiting."
     exit 1
   fi
 else
-  echo "B2 backup is disabled. Skipping the download and using the local backup."
+  echo "S3 backup is disabled. Skipping the download and using the local backup."
   BACKUP_FILE_TO_RESTORE=$(ls -t $ETCD_BACKUP_DIR | head -n 1)
 fi
 
